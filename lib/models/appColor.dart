380e42b6602659ce0fa9656c4578e09ffc5b8eec
digitalartist221 <sall.aboubacre@gmail.com>
2024-11-29 01:02:42 +0100

    final commit

--- lib/models/appColor.dart
+++ lib/models/appColor.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color backgroundDark = Color(0xFF121212); // Fond sombre
   static const Color backgroundLight = Color(0xFFFFF8E1); // Beige clair
-  static const Color accentOrange = Color(0xFFFF7043); // Orange vif
+  static const Color accentOrange =
+      Color.fromRGBO(255, 112, 67, 1); // Orange vif
   static const Color accentYellow = Color(0xFFFFF176); // Jaune clair
   static const Color accentBlue = Color(0xFF42A5F5); // Bleu clair
   static const Color accentRed = Color(0xFFEF5350); // Rouge clair
